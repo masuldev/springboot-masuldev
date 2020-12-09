88d020caca2bbd7ba66ee0996210036e0e104dca
In Kim <kimmoimm@gmail.com>
2020-12-09 23:53:35 +0900

    add naver login and verification test passed TODO::need to hello test

--- src/test/java/com/masuldev/spring/web/IndexControllerTest.java
+++ src/test/java/com/masuldev/spring/web/IndexControllerTest.java
@@ -23,6 +23,6 @@ public class IndexControllerTest {
         String body = this.restTemplate.getForObject("/", String.class);
 
         //then
-        assertThat(body).contains("SpringBoot Web Service");
+        assertThat(body).contains("SpringBoot!!");
     }
 }
