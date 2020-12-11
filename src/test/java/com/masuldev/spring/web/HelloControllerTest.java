a132c5ec33bcb0e357154f47be3e155e81f8c988
In Kim <kimmoimm@gmail.com>
2020-12-11 17:48:37 +0900

    modify error test code

---
+++ src/test/java/com/masuldev/spring/web/HelloControllerTest.java
@@ -0,0 +1,37 @@
+package com.masuldev.spring.web;
+
+import com.masuldev.spring.config.auth.SecurityConfig;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.FilterType;
+import org.springframework.security.test.context.support.WithMockUser;
+import org.springframework.test.context.junit4.SpringRunner;
+import org.springframework.test.web.servlet.MockMvc;
+
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+@RunWith(SpringRunner.class)
+@WebMvcTest(controllers = HelloController.class,
+        excludeFilters = {
+        @ComponentScan.Filter(type = FilterType.ASSIGNABLE_TYPE, classes = SecurityConfig.class)
+        })
+public class HelloControllerTest {
+
+    @Autowired
+    private MockMvc mvc;
+
+    @WithMockUser(roles = "USER")
+    @Test
+    public void hello가_리턴된다() throws Exception{
+        String hello = "hello";
+
+        mvc.perform(get("/hello"))
+                .andExpect(status().isOk())
+                .andExpect(content().string(hello));
+    }
+}
---
+++ src/test/java/com/masuldev/spring/web/HelloResponseDtoTest.java
@@ -0,0 +1,26 @@
+package com.masuldev.spring.web;
+
+import com.masuldev.spring.web.dto.HelloResponseDto;
+import org.junit.Test;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+public class HelloResponseDtoTest {
+
+    @Test
+    public void 롬북_기능_테스트() throws Exception{
+        //given
+        String name = "test";
+        int amount = 1000;
+
+        //when
+        HelloResponseDto dto = new HelloResponseDto(name, amount);
+
+        //then
+        assertThat(dto.getName()).isEqualTo(name);
+        assertThat(dto.getAmount()).isEqualTo(amount);
+
+
+
+    }
+}
