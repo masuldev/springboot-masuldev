290b2ceedce4f8f16de4e69b9d91f264b8b2dec1
In Kim <kimmoimm@gmail.com>
2020-12-13 02:36:15 +0900

    modify deploy.sh

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 REPOSITORY=/home/ec2-user/app/step2
-PROJECT_NAME=com.masuldev.springboot
+PROJECT_NAME=com.masuldev.spring
 
 echo "> Build 파일 복사"
 cp $REPOSITORY/zip/*.jar $REPOSITORY/
