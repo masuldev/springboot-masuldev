c59e457cfb8d314ab4372ac097b317b96621bef4
In Kim <kimmoimm@gmail.com>
2020-12-26 21:42:59 +0900

    update Gradle version

--- src/test/java/com/masuldev/spring/web/HelloResponseDtoTest.java
+++ src/test/java/com/masuldev/spring/web/HelloResponseDtoTest.java
@@ -1,7 +1,7 @@
 package com.masuldev.spring.web;
 
 import com.masuldev.spring.web.dto.HelloResponseDto;
-import org.junit.Test;
+import org.junit.jupiter.api.Test;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
